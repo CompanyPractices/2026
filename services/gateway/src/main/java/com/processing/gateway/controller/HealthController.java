66208948f21a68b8c15620c8902ac3799b3d14b6
Igor Nasonkov <inasonkov@openintegration.inc>
2026-06-02 19:05:41 +0300

    fix: fixed health-check

--- services/gateway/src/main/java/com/processing/gateway/controller/HealthController.java
+++ services/gateway/src/main/java/com/processing/gateway/controller/HealthController.java
@@ -2,6 +2,7 @@ package com.processing.gateway.controller;
 
 import com.processing.gateway.dto.HealthResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +15,17 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class HealthController {
     private final HttpClient httpClient;
     // private final WebClient webClient;
 
     @GetMapping("/health")
     public ResponseEntity<HealthResponse> health() {
-        String switchUrl = System.getenv("SWITCH");
-        String loggerUrl = System.getenv("LOGGER");
-        String authUrl = System.getenv("AUTH");
-        String cardsUrl = System.getenv("CARDS");
+        String switchUrl = System.getenv("SWITCH") + "/health";
+        String loggerUrl = System.getenv("LOGGER") + "/health";
+        String authUrl = System.getenv("AUTH") + "/health";
+        String cardsUrl = System.getenv("CARDS") + "/health";
 
         return ResponseEntity.ok(new HealthResponse(
                 "ok",
@@ -43,7 +45,7 @@ public class HealthController {
                     .uri(URI.create(url))
                     .build(), HttpResponse.BodyHandlers.ofString());
 
-            if (response.body().contains("\"status\": \"ok\""))
+            if (response.body().contains("\"status\":\"ok\""))
                 return "ok";
 
             return "down";
