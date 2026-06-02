fd85e26934ebe341704f058e01f708131b951317
Nikita Kapustkin <nkapustkin@openintegration.inc>
2026-06-02 19:31:14 +0300

    feat(logger-search): add Transaction entity

---
+++ services/transaction-logger/src/main/java/com/processing/model/Transaction.java
@@ -0,0 +1,35 @@
+package com.processing.model;
+
+import com.processing.enums.TransactionStatus;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    private UUID id;
+    private String mti;
+    private String stan;
+    private String rrn;
+    private String pan;
+    private String processingCode;
+    private Long amount;
+    private String currencyCode;
+    private String terminalId;
+    private String merchantId;
+    private String mcc;
+    private String acquirerId;
+    private String issuerId;
+    private Long acquiringFee;
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+    private String declineReason;
+    private String authCode;
+    private Instant transmissionDateTime;
+    private Instant createdAt;
+
+}
