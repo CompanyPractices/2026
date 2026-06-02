0222bc69ad3eebeb7305f90bf294ffdeac0cf6fc
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-02 18:15:35 +0300

    refactor(card-management): new folders architecture

--- services/card-management/src/main/java/com/processing/controllers/HealthController.java
+++ services/card-management/src/main/java/com/processing/controllers/HealthController.java
@@ -1,6 +1,6 @@
-package com.processing.controller;
+package com.processing.controllers;
 
-import com.processing.dto.HealthResponse;
+import com.processing.models.HealthResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- services/card-management/src/main/java/com/processing/models/HealthResponse.java
+++ services/card-management/src/main/java/com/processing/models/HealthResponse.java
@@ -1,4 +1,4 @@
-package com.processing.dto;
+package com.processing.models;
 
 import java.util.Map;
 
