cf58aff7e9f0fd3964396a542688deedb8e7097c
Igor Nasonkov <igornasonkov@gmail.com>
2026-06-03 17:31:18 +0300

    refactor(gateway): changed health-check moved http calls to service layer added configuration properties changed env variables names to correct ones changed health-check logic to look on response status and parse body with jackson

--- services/gateway/src/main/java/com/processing/gateway/controller/HealthController.java
+++ services/gateway/src/main/java/com/processing/gateway/controller/HealthController.java
@@ -1,55 +1,26 @@
 package com.processing.gateway.controller;
 
 import com.processing.gateway.dto.HealthResponse;
+import com.processing.gateway.service.HealthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class HealthController {
-    private final HttpClient httpClient;
+    private final HealthService healthService;
 
     @GetMapping("/health")
     public ResponseEntity<HealthResponse> health() {
-        String switchUrl = System.getenv("SWITCH") + "/health";
-        String loggerUrl = System.getenv("LOGGER") + "/health";
-        String authUrl = System.getenv("AUTH") + "/health";
-        String cardsUrl = System.getenv("CARDS") + "/health";
-
         return ResponseEntity.ok(new HealthResponse(
                 "ok",
                 "gateway",
                 "1.0.0",
-                Map.of("switch", checkService(switchUrl),
-                        "authorization", checkService(authUrl),
-                        "cardManagement", checkService(cardsUrl),
-                        "logger", checkService(loggerUrl))
+                healthService.getDownstreamServicesHealth()
         ));
-    }
-
-    private String checkService(String url) {
-        try {
-            var response = httpClient.send(HttpRequest.newBuilder()
-                    .GET()
-                    .uri(URI.create(url))
-                    .build(), HttpResponse.BodyHandlers.ofString());
-
-            if (response.body().contains("\"status\":\"ok\""))
-                return "ok";
-
-            return "down";
-        } catch (Exception e) {
-            return "down";
-        }
     }
 }
