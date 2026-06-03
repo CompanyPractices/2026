e6180898a8d00463884ac4f4b1258d586d05764f
daniil3006 <daniil.stank@gmail.com>
2026-06-03 14:43:47 +0300

    fix(card-management): add @PostMapping and @Valid to generate endpoint

--- services/card-management/src/main/java/com/processing/controllers/CardGeneratorController.java
+++ services/card-management/src/main/java/com/processing/controllers/CardGeneratorController.java
@@ -5,8 +5,10 @@ import com.processing.models.CardEntity;
 import com.processing.models.GenerateCardResponse;
 import com.processing.models.GenerateCardsRequest;
 import com.processing.services.CardGeneratorService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,8 @@ import java.util.List;
 public class CardGeneratorController {
     private final CardGeneratorService generatorService;
 
-    public ResponseEntity<GenerateCardResponse> generate(@RequestBody GenerateCardsRequest request) {
+    @PostMapping("/generate")
+    public ResponseEntity<GenerateCardResponse> generate(@Valid @RequestBody GenerateCardsRequest request) {
         List<CardEntity> cards = generatorService.generate(request.getCount(), request.getBins());
         List<CardDto> result = cards.stream()
                 .map(CardDto::fromEntity)
