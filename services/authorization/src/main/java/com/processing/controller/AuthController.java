8102c53ab7bb62bcbbcc0141133393a4a4caecad
Alina Klimenko <aklimenko@openintegration.inc>
2026-06-03 16:36:43 +0300

    feat: add rnn and authcode generators

--- services/authorization/src/main/java/com/processing/controller/AuthController.java
+++ services/authorization/src/main/java/com/processing/controller/AuthController.java
@@ -8,8 +8,15 @@ import java.time.LocalDate;
 import com.processing.enums.CardStatus;
 import com.processing.dto.*;
 import java.lang.Exception;
+import java.util.Calendar;
+import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
 
     private HttpClient httpClient = HttpClient.newHttpClient();
@@ -29,12 +36,11 @@ public class AuthController {
             };
         }
 
+        if (LocalDate.parse(cardResponse.getExpiryDate()).isBefore(LocalDate.now())) {
+            return AuthorizationResponse.declined(request, "CARD_EXPIRED", "54");
+        }
 
-         if (LocalDate.parse(cardResponse.getExpiryDate()).isBefore(LocalDate.now())) {
-             return AuthorizationResponse.declined(request, "CARD_EXPIRED", "54");
-         }
-
-         // TODO check daily limit when add table limit_usage
+        // TODO check daily limit when add table limit_usage
 
         // TODO check month limit when add table limit_usage
 
@@ -42,45 +48,64 @@ public class AuthController {
             return AuthorizationResponse.declined(request, "INSUFFICIENT_FUNDS", "51");
 
         reserve();
-        generateRnn();
-        generateAuthCode();
-        return AuthorizationResponse.approved(request, "", ""); // Заглушка
+        String rnn = generateRNN();
+        String authCode = generateAuthCode();
+        return AuthorizationResponse.approved(request, rnn, authCode);
     }
 
     private CardResponse getCard(String pan) throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(cmsUrl + "/api/cards/" + pan))
-            .GET()
-            .build();
- 
+                .uri(URI.create(cmsUrl + "/api/cards/" + pan))
+                .GET()
+                .build();
+
         HttpResponse<String> cardResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
- 
+
         // заглушка
         return new CardResponse(
-            UUID.randomUUID(), 
-            "1234123412341234", 
-            "400000",
-            "IVAN IVANOV",
-            "1228",
-            CardStatus.ACTIVE,
-            "643",
-            15000000,
-            300000000,
-            100000000,
-            "ISS001",
-            LocalDate.parse("2026-06-01T10:00:00")
-        );
+                UUID.randomUUID(),
+                "1234123412341234",
+                "400000",
+                "IVAN IVANOV",
+                "1228",
+                CardStatus.ACTIVE,
+                "643",
+                15000000,
+                300000000,
+                100000000,
+                "ISS001",
+                LocalDate.parse("2026-06-01T10:00:00"));
     }
 
     void reserve() {
         // TODO
     }
 
-    void generateRnn() {
-        // TODO
+    private static final AtomicInteger counter = new AtomicInteger(0);
+    private static String lastSecond = "";
+
+    private String generateRNN() {
+        Calendar calendar = Calendar.getInstance();
+
+        String currentSecond = String.format("%1d%03d%02d%02d%02d",
+                calendar.get(Calendar.YEAR) % 10,
+                calendar.get(Calendar.DAY_OF_YEAR),
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                calendar.get(Calendar.SECOND));
+
+        if (!currentSecond.equals(lastSecond)) {
+            lastSecond = currentSecond;
+            counter.set(0);
+        }
+
+        int sequence = counter.getAndIncrement() % 10;
+        return currentSecond + sequence;
     }
 
-    void generateAuthCode() {
-        // TODO
+    private String generateAuthCode() {
+        return new Random().ints(6, 0, 36)
+                .mapToObj(i -> Character.toString(i < 10 ? '0' + i : 'A' + i - 10))
+                .collect(Collectors.joining());
     }
 }
