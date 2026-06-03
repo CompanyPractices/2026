a08117df2b4e2926cf72c2ee92d0d9dd7137b266
Nikita Kapustkin <nkapustkin@openintegration.inc>
2026-06-03 17:23:23 +0300

    feat(logger-search): add WebSocket manager, handler and config

---
+++ services/transaction-logger/src/main/java/com/processing/websocket/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.processing.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final TransactionWebSocketHandler handler;
+
+    @Override
+    public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler, "/ws/transactions").setAllowedOrigins("*");
+    }
+}
