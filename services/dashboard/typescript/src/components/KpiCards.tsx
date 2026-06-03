73ae88d942f121cba2fe692f08b742b60ae679bd
KulakovaY <408948@niuitmo.ru>
2026-06-03 10:40:02 +0300

    refactor: types for KpiCards in one place

--- services/dashboard/typescript/src/components/KpiCards.tsx
+++ services/dashboard/typescript/src/components/KpiCards.tsx
@@ -1,4 +1,12 @@
-import { KpiCardsProps } from "./types";
+type KpiCardData = {
+    label: string;
+    value: number | string;
+    unit?: string;
+};
+
+export type KpiCardsProps = {
+    cards: KpiCardData[];
+};
 
 export function KpiCards( { cards } : KpiCardsProps) {
     return <div>
--- services/dashboard/typescript/src/components/KpiCards/types.ts
+++
@@ -1,9 +0,0 @@
-type KpiCardData = {
-    label: string;
-    value: number | string;
-    unit?: string;
-};
-
-export type KpiCardsProps = {
-    cards: KpiCardData[];
-};
