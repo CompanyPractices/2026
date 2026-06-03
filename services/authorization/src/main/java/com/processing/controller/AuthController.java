9f7339a3b83f57fa88b9f8b1a505703a2fb285eb
Daria Ermolaeva <dermolaeva@openintegration.inc>
2026-06-03 15:28:05 +0300

    Added dtos, enums and authController

---
+++ services/authorization/src/main/java/com/processing/controller/AuthController.java
@@ -0,0 +1,86 @@
+package com.processing.controller;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import com.processing.enums.CardStatus;
+import com.processing.dto.*;
+import java.lang.Exception;
+import java.util.UUID;
+
+public class AuthController {
+
+    private HttpClient httpClient = HttpClient.newHttpClient();
+
+    private final String cmsUrl = "http://localhost:3000";
+
+    public AuthorizationResponse authorize(AuthorizationRequest request) throws Exception {
+        CardResponse cardResponse = getCard(request.getPan());
+
+        CardStatus currCardStatus = cardResponse.getStatus();
+        if (!currCardStatus.equals(CardStatus.ACTIVE)) {
+            return switch (currCardStatus) {
+                case EXPIRED -> AuthorizationResponse.declined(request, "CARD_EXPIRED", "54");
+                case BLOCKED -> AuthorizationResponse.declined(request, "CARD_BLOCKED", "05");
+                case INACTIVE -> AuthorizationResponse.declined(request, "CARD_INACTIVE", "05");
+                default -> AuthorizationResponse.declined(request, "UNKNOWN_REASON", "05");
+            };
+        }
+
+
+         if (LocalDate.parse(cardResponse.getExpiryDate()).isBefore(LocalDate.now())) {
+             return AuthorizationResponse.declined(request, "CARD_EXPIRED", "54");
+         }
+
+         // TODO check daily limit when add table limit_usage
+
+        // TODO check month limit when add table limit_usage
+
+        if (request.getAmount() > cardResponse.getAvailableBalance())
+            return AuthorizationResponse.declined(request, "INSUFFICIENT_FUNDS", "51");
+
+        reserve();
+        generateRnn();
+        generateAuthCode();
+        return AuthorizationResponse.approved(request, "", ""); // Заглушка
+    }
+
+    private CardResponse getCard(String pan) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(cmsUrl + "/api/cards/" + pan))
+            .GET()
+            .build();
+ 
+        HttpResponse<String> cardResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+ 
+        // заглушка
+        return new CardResponse(
+            UUID.randomUUID(), 
+            "1234123412341234", 
+            "400000",
+            "IVAN IVANOV",
+            "1228",
+            CardStatus.ACTIVE,
+            "643",
+            15000000,
+            300000000,
+            100000000,
+            "ISS001",
+            LocalDate.parse("2026-06-01T10:00:00")
+        );
+    }
+
+    void reserve() {
+        // TODO
+    }
+
+    void generateRnn() {
+        // TODO
+    }
+
+    void generateAuthCode() {
+        // TODO
+    }
+}
