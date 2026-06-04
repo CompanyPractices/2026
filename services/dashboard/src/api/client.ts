c016acf53ccf98b686d4b4a38555100817054821
Alina Ramazanova <alinaramazanova610@gmail.com>
2026-06-04 17:59:15 +0300

    feat(web-dashboard): add api client

--- services/dashboard/src/api/client.ts
+++ services/dashboard/src/api/client.ts
@@ -1,1 +1,12 @@
-export {};
+const base_url = "http://localhost:8080";
+
+function fetchApi<T>(route: string): Promise<T> {
+    return fetch(base_url + route)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json()
+        })
+        .catch(error => {throw new Error(`Error status: ${error.message}`)})
+}
