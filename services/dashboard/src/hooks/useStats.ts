20909f97ac0dbc1a930a69d68776a9b1f85d2595
Alina Ramazanova <alinaramazanova610@gmail.com>
2026-06-04 16:29:55 +0300

    merge feature/web-dashboard/UI-2 into feature/web-dashboard/UI-6

--- services/dashboard/src/hooks/useStats.ts
+++ services/dashboard/src/hooks/useStats.ts
@@ -1,1 +1,12 @@
-export {};
+import React, {useState, useEffect} from "react";
+
+function useStats() {
+    const [totalTransactions, setTotalTransactions] = useState(0);
+    const [approvalRate, setApprovalRate] = useState(0);
+    const [totalAmount, setTotalAmount] = useState(0);
+    const [avgTime, setAvgTime] = useState(0);
+
+    useEffect(() => {
+        fetchStats()
+    }
+}
