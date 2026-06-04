e60482d45fb9be863871deddd7e5c3f5fe68ca59
KulakovaY <408948@niuitmo.ru>
2026-06-04 18:11:34 +0300

    feat: table styles corrected

--- services/dashboard/src/components/TransactionTable/TransactionTable.tsx
+++ services/dashboard/src/components/TransactionTable/TransactionTable.tsx
@@ -9,16 +9,16 @@ type TransactionTableProps = {
 export function TransactionTable({ transactions } : TransactionTableProps ){
     return (
         <div className="font-mono flex flex-col items-center">
-            <h2 className="text-2xl font-bold mb-6 text-center drop-shadow-lg ">Последние 20 транзакций</h2>
+            <h2 className="text-2xl font-bold mb-3 text-center drop-shadow-lg ">Последние 20 транзакций</h2>
 
-            <div className="rounded-3xl overflow-hidden border-2 border-gray-400 min-w-1/2 max-w-3/4 shadow-lg">
+            <div className="rounded-3xl overflow-hidden border-2 border-emerald-600 min-w-1/2 max-w-3/4 shadow-lg mb-5">
                 <table className="table-auto w-full">
-                    <tr className=" border-b-2 border-gray-400 text-center text-semibold bg-gray-50">
+                    <tr className=" border-b-2 border-emerald-600 text-center text-semibold bg-gray-50">
                         <th className="px-10 py-5"> Время </th>
-                        <th className="px-10 py-2"> PAN </th>
-                        <th className="px-10 py-2"> Сумма </th>
-                        <th className="px-10 py-2"> Мерчант </th>
-                        <th className="px-10 py-2"> Статус </th>
+                        <th className="px-10 py-5"> PAN </th>
+                        <th className="px-10 py-5"> Сумма </th>
+                        <th className="px-10 py-5"> Мерчант </th>
+                        <th className="px-10 py-5"> Статус </th>
                     </tr>
                     {transactions.map((transaction) => {
                         const statusIconData = getStatusIcon(transaction.status);
