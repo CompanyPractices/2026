e8f5c4701614a04ac8d639d29b60d54c213c1f98
KulakovaY <408948@niuitmo.ru>
2026-06-04 14:42:31 +0300

    feat: utils for formatting are done

---
+++ services/dashboard/src/utils/format.ts
@@ -0,0 +1,22 @@
+import { formatInTimeZone } from 'date-fns-tz';
+import { ru } from 'date-fns/locale';
+
+export const hidePan = (pan: string): string => {
+    return `${pan.slice(0, 4)} **** ${pan.slice(-4)}`;
+};
+
+export const convertPenniesToRubles = (pennies: number): string => {
+    const rubles = pennies / 100;
+    return rubles.toLocaleString('ru-RU', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).replace(',', '.') + ' ₽';
+};
+
+export const formatTime = (iso: string): string => {
+    return formatInTimeZone(iso, 'UTC','HH:mm:ss');
+};
+
+export const formatDateTime = (iso: string): string => {
+    return formatInTimeZone(iso, 'UTC','dd.MM.yyyy, HH:mm:ss', { locale: ru });
+};
