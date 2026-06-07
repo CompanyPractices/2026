36d02c6b748da7f6e60a402191651e9361b4185e
Bushkov Nikita <408338@niuitmo.ru>
2026-06-07 15:39:11 +0300

    fix(checkstyle): fix code style violations to fix pipeline

--- services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/domain/model/AuthorizationRequest.java
+++ services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/domain/model/AuthorizationRequest.java
@@ -1,25 +1,19 @@
 package com.processing.merchantacquirer.domain.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-
-import java.time.LocalDateTime;
 
 @Builder
 public record AuthorizationRequest(
-        String mti,// "0100" = Auth Request, всегда одинаковый
-        String stan,// System Trace Audit Number, написать реализацию генерации
+        String mti, // "0100" = Auth Request, всегда одинаковый
+        String stan, // System Trace Audit Number, написать реализацию генерации
         String pan, //card
         String processingCode, //"000000" = покупка
-        int amount,//Сумма в копейках
+        int amount, //Сумма в копейках
         String currencyCode, //"643" - идентификатор валюты (рубли), есть в CardsResponse
-        String transmissionDateTime,// Время отправки
-        String terminalId,// terminal ?
-        String terminalType,// terminal ?
-        String merchantId,// merchant
+        String transmissionDateTime, // Время отправки
+        String terminalId, // terminal ?
+        String terminalType, // terminal ?
+        String merchantId, // merchant
         String mcc, // merchant
         String acquirerId// merchant
 ) {
