49f6fb9a90e626fc6a5fc265b401ff1ddaae757e
KulakovaY <408948@niuitmo.ru>
2026-06-08 17:29:42 +0300

    fix: merge conflicts

--- services/dashboard/src/App.tsx
+++ services/dashboard/src/App.tsx
@@ -2,12 +2,11 @@ import LineChart from "./components/LineChart.tsx";
 import {Header} from "./components/Header.tsx";
 import PieChart from "./components/PieChart.tsx";
 import {TransactionTable} from "./components/TransactionTable.tsx";
-import {MOCK_DASHBOARD_STATS} from "./mockData.ts";
 
 function App() {
   return (
     <div className="bg-zinc-200 min-h-screen flex flex-col items-center justify-items-stretch">
-      <Header stats={MOCK_DASHBOARD_STATS} />
+      <Header />
       <main className="w-2/3 flex-grow grid grid-cols-4 gap-4">
           <div className="col-span-2 bg-zinc-300 m-4 rounded-lg shadow-xl place-content-center">
               <LineChart />
