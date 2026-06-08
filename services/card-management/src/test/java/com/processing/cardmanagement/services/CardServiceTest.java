b4168b92bfddc48f323d2348cbf38d1fc1a2f2f8
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-08 10:27:44 +0300

    feat(card-management): first crud test

--- services/card-management/src/test/java/com/processing/cardmanagement/services/CardServiceTest.java
+++ services/card-management/src/test/java/com/processing/cardmanagement/services/CardServiceTest.java
@@ -1,56 +1,32 @@
 package com.processing.cardmanagement.services;
 
-import com.processing.cardmanagement.events.CardEventNotifier;
-import com.processing.cardmanagement.exceptions.CardNotFoundException;
-import com.processing.cardmanagement.exceptions.InsufficientFundsException;
-import com.processing.cardmanagement.models.Card;
-import com.processing.cardmanagement.models.CardStatus;
-import com.processing.cardmanagement.options.CardServiceDefaults;
-import com.processing.cardmanagement.options.CardServiceDefaultsConfigurationProperties;
-import com.processing.cardmanagement.options.CardServiceSettings;
-import com.processing.cardmanagement.options.CardServiceSettingsConfigurationProperties;
+import com.processing.cardmanagement.models.CardEntity;
+import com.processing.cardmanagement.options.CardServiceOptions;
 import com.processing.cardmanagement.repositories.CardRepository;
+import com.processing.common.dto.cardmanagement.CreateCardRequest;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
-import java.time.YearMonth;
-import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 import java.util.UUID;
-import java.util.function.UnaryOperator;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public final class CardServiceTest {
 
-    private final String panGeneratorCardNumber = "1234 5678 9101 1123".replace(" ", "");
+    private final String testCardNumber = "1234 5678 9101 1123".replace(" ", "");
 
     private final Faker faker = new Faker(Locale.ENGLISH);
 
-    private final CardServiceSettings settings = new CardServiceSettingsConfigurationProperties(
-        "TESTISSUER",
-        3
-    );
-
-    private final CardServiceDefaults defaults = new CardServiceDefaultsConfigurationProperties(
-        1,
-        50,
-        "643",
-        15000000,
-        300000000,
-        1000000
-    );
+    private final CardServiceOptions options = new CardServiceOptions("TESTISSUER");
 
     private final PanGenerator panGenerator = new PanGenerator() {
         @Override
@@ -60,271 +36,42 @@ public final class CardServiceTest {
 
         @Override
         public String generatePan(String bin) {
-            return panGeneratorCardNumber;
+            return testCardNumber;
         }
     };
-
-    private final ArgumentCaptor<Card> cardCaptor =
-        ArgumentCaptor.forClass(Card.class);
 
     @Mock
     private CardRepository cardRepository;
 
-    @Mock
-    private CardEventNotifier eventNotifier;
-
+    @InjectMocks
     private CardService cardService;
 
     @BeforeEach
     void setUp() {
-        cardService = new CardServiceImpl(
+        cardService = new CardService(
             cardRepository,
-            settings,
-            defaults,
-            panGenerator,
-            eventNotifier
+            options,
+            panGenerator
         );
     }
 
     @Test
     void testCreateCard() {
-        var bin = faker.number().digits(6);
-        var cardholderName = faker.name().fullName().toUpperCase(Locale.ROOT);
-        var currencyCode = faker.number().digits(3);
-        var dailyLimit = faker.number().numberBetween(0L, 10000000L);
-        var monthlyLimit = faker.number().numberBetween(dailyLimit, 30000000L);
-        var initialBalance = faker.number().numberBetween(0L, 10000000L);
-        var expDate = YearMonth.now().plusYears(settings.cardValidityPeriod());
+        when(cardRepository.save(any(CardEntity.class))).thenAnswer(invocation -> {
+            var data = (CardEntity) invocation.getArgument(0);
+            data.setId(UUID.randomUUID());
+            return data;
+        });
 
-        when(cardRepository.save(any(Card.class)))
-            .thenAnswer(invocation -> invocation.getArgument(0));
-
-        var response = cardService.createCard(
-            bin,
-            cardholderName,
-            currencyCode,
-            dailyLimit,
-            monthlyLimit,
-            initialBalance
-        );
-
-        var expected = new Card(
-            response.id(),
-            panGeneratorCardNumber,
-            bin,
-            cardholderName,
-            expDate,
-            CardStatus.ACTIVE,
-            currencyCode,
-            dailyLimit,
-            monthlyLimit,
-            initialBalance,
-            settings.issuerId(),
-            response.createdAt()
-        );
-
-        assertEquals(expected, response);
-    }
-
-    @Test
-    void testGetCard() {
-        var pan = generatePan();
-
-        var card = createTestCard(pan);
-        when(cardRepository.findByPan(pan)).thenReturn(Optional.of(card));
-
-        var found = cardService.getCard(pan);
-        assertEquals(card, found);
-
-        when(cardRepository.findByPan(anyString())).thenReturn(Optional.empty());
-        assertThrows(CardNotFoundException.class, () -> cardService.getCard(pan));
-    }
-
-    @Test
-    void testGetCards() {
-        var testCard = createTestCard(generatePan());
-
-        int limit = faker.number().numberBetween(1, 100);
-        int offset = faker.number().numberBetween(0, 100);
-        CardStatus status = CardStatus.ACTIVE;
-        String bin = faker.number().digits(6);
-        String issuerId = faker.regexify("[A-Z0-9]{1,10}");
-        LocalDateTime startDate = LocalDateTime.now().minusDays(1);
-        LocalDateTime endDate = LocalDateTime.now();
-
-        when(cardRepository.findCards(
-            limit,
-            offset,
-            status,
-            bin,
-            issuerId,
-            startDate,
-            endDate
-        )).thenReturn(List.of(testCard));
-
-        var cards = cardService.getCards(limit, offset, status, bin, issuerId, startDate, endDate);
-        assertEquals(1, cards.size());
-        assertEquals(testCard, cards.getFirst());
-    }
-
-    @Test
-    void testPatchCard() {
-        var pan = generatePan();
-        var status = CardStatus.EXPIRED;
-        var dailyLimit = faker.number().numberBetween(0L, 10000000L);
-        var monthlyLimit = faker.number().numberBetween(dailyLimit, 30000000L);
-        var availableBalance = faker.number().numberBetween(0L, 10000000L);
-        var testCard = createTestCard(pan);
-
-        when(cardRepository.updateWithPessimisticLock(eq(pan), any()))
-            .thenAnswer(invocation -> {
-                @SuppressWarnings("unchecked")
-                var func = (UnaryOperator<Card>) invocation.getArgument(1, UnaryOperator.class);
-                return func.apply(testCard);
-            });
-
-        var expected = new Card(
-            testCard.id(),
-            testCard.pan(),
-            testCard.bin(),
-            testCard.cardholderName(),
-            testCard.expiryDate(),
-            status,
-            testCard.currencyCode(),
-            dailyLimit,
-            monthlyLimit,
-            availableBalance,
-            testCard.issuerId(),
-            testCard.createdAt()
-        );
-
-        var card = cardService.patchCard(
-            pan,
-            status,
-            dailyLimit,
-            monthlyLimit,
-            availableBalance
-        );
-        assertEquals(expected, card);
-    }
-
-    @Test
-    void testDeleteCard() {
-        var pan = generatePan();
-        var testCard = createTestCard(pan);
-        when(cardRepository.findByPan(pan)).thenReturn(Optional.of(testCard));
-        when(cardRepository.save(any(Card.class)))
-            .thenAnswer(invocation -> invocation.getArgument(0));
-
-        var expected = new Card(
-            testCard.id(),
-            testCard.pan(),
-            testCard.bin(),
-            testCard.cardholderName(),
-            testCard.expiryDate(),
-            CardStatus.DELETED,
-            testCard.currencyCode(),
-            testCard.dailyLimit(),
-            testCard.monthlyLimit(),
-            testCard.availableBalance(),
-            testCard.issuerId(),
-            testCard.createdAt()
-        );
-
-        cardService.deleteCard(pan);
-        verify(cardRepository, times(1)).save(cardCaptor.capture());
-        var deletedCard = cardCaptor.getValue();
-        assertEquals(expected, deletedCard);
-    }
-
-    @Test
-    void testCountCardsFiltered() {
-        var amount = 1L;
-        CardStatus status = CardStatus.ACTIVE;
-        String bin = faker.number().digits(6);
-        String issuerId = faker.regexify("[A-Z0-9]{1,10}");
-        LocalDateTime startDate = LocalDateTime.now().minusDays(1);
-        LocalDateTime endDate = LocalDateTime.now();
-
-        when(cardRepository.countCardsFiltered(
-            status,
-            bin,
-            issuerId,
-            startDate,
-            endDate
-        )).thenReturn(amount);
-
-        var count = cardService.countCardsFiltered(
-            status,
-            bin,
-            issuerId,
-            startDate,
-            endDate
-        );
-        assertEquals(amount, count);
-    }
-
-    @Test
-    void testCountAllCards() {
-        var returnValue = 1L;
-        when(cardRepository.countAllCards()).thenReturn(returnValue);
-        assertEquals(returnValue, cardService.countAllCards());
-    }
-
-    @Test
-    void testReserve() {
-        var pan = generatePan();
-        var testCard = createTestCard(pan);
-        var reserveAmount = faker.number().numberBetween(0L, testCard.availableBalance());
-        when(cardRepository.updateWithPessimisticLock(eq(pan), any()))
-            .thenAnswer(invocation -> {
-                @SuppressWarnings("unchecked")
-                var func = (UnaryOperator<Card>) invocation.getArgument(1, UnaryOperator.class);
-                return func.apply(testCard);
-            });
-
-        var expected = new Card(
-            testCard.id(),
-            testCard.pan(),
-            testCard.bin(),
-            testCard.cardholderName(),
-            testCard.expiryDate(),
-            testCard.status(),
-            testCard.currencyCode(),
-            testCard.dailyLimit(),
-            testCard.monthlyLimit(),
-            testCard.availableBalance() - reserveAmount,
-            testCard.issuerId(),
-            testCard.createdAt()
-        );
-
-        var card = cardService.reserve(pan, reserveAmount);
-        assertEquals(expected, card);
-        assertThrows(InsufficientFundsException.class, () ->
-            cardService.reserve(
-                pan,
-                Long.MAX_VALUE
-            )
-        );
-    }
-
-    private Card createTestCard(String pan) {
-        return new Card(
-            UUID.randomUUID(),
-            pan,
-            pan.substring(0, 6),
+        var request = new CreateCardRequest(
+            faker.number().digits(6),
             faker.name().fullName().toUpperCase(Locale.ROOT),
-            YearMonth.now().plusYears(settings.cardValidityPeriod()),
-            CardStatus.ACTIVE,
-            defaults.currencyCode(),
-            defaults.dailyLimit(),
-            defaults.monthlyLimit(),
-            defaults.balance(),
-            settings.issuerId()
+            faker.number().digits(3),
+            faker.number().numberBetween(0, 10000000),
+            faker.number().numberBetween(0, 30000000),
+            faker.number().numberBetween(0, 10000000)
         );
-    }
-
-    private String generatePan() {
-        return faker.regexify("[0-9]{16}");
+        cardService.createCard(request);
+        verify(cardRepository, times(1)).save(any(CardEntity.class));
     }
 }
