bfb5d93e3d20db0804798b814f43d502088854e8
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-08 09:52:27 +0300

    refactor(card-management): made soft deletion via SQLDelete annotation
    # Conflicts:
    #	services/card-management/src/main/java/com/processing/cardmanagement/models/CardEntity.java
    #	services/card-management/src/main/java/com/processing/cardmanagement/services/CardService.java

--- services/card-management/src/main/java/com/processing/cardmanagement/services/CardService.java
+++ services/card-management/src/main/java/com/processing/cardmanagement/services/CardService.java
@@ -1,111 +1,151 @@
 package com.processing.cardmanagement.services;
 
 import com.processing.cardmanagement.exceptions.CardNotFoundException;
-import com.processing.cardmanagement.models.Card;
-import com.processing.cardmanagement.models.CardDraft;
-import com.processing.cardmanagement.models.CardStatus;
-import jakarta.annotation.Nullable;
+import com.processing.cardmanagement.models.*;
+import com.processing.cardmanagement.options.CardServiceOptions;
+import com.processing.cardmanagement.repositories.CardRepository;
+import com.processing.common.dto.cardmanagement.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CardService {
 
-public interface CardService {
+    private final CardRepository cardRepository;
+    private final CardServiceOptions options;
+    private final PanGenerator panGenerator;
 
-    Card createCard(
+    public CardModel createCard(CreateCardRequest data) {
+        var entity = cardRepository.save(
+            new CardEntity(
+                panGenerator.generatePan(data.bin()),
+                data.bin(),
+                data.cardholderName(),
+                data.currencyCode(),
+                CardStatus.ACTIVE,
+                data.dailyLimit(),
+                data.monthlyLimit(),
+                data.initialBalance(),
+                options.issuerId()
+            )
+        );
+        return cardModelFromEntity(entity);
+    }
+
+    public List<CardModel> createCards(List<GeneratedCardDto> data) {
+        var entities = data
+            .stream()
+            .map(dto -> new CardEntity(
+                panGenerator.generatePan(dto.bin()),
+                dto.bin(),
+                dto.cardholderName(),
+                dto.currencyCode(),
+                dto.status(),
+                dto.dailyLimit(),
+                dto.monthlyLimit(),
+                dto.balance(),
+                options.issuerId()
+            ))
+            .toList();
+
+        return cardRepository
+            .saveAll(entities)
+            .stream()
+            .map(this::cardModelFromEntity)
+            .toList();
+    }
+
+    public CardModel getCard(String pan) {
+        return cardModelFromEntity(getCardEntity(pan));
+    }
+
+    public GetCardsResponse getCards(
+        Integer limit,
+        Integer offset,
+        CardStatus status,
         String bin,
-        String cardholderName,
-        String currencyCode,
-        long dailyLimit,
-        long monthlyLimit,
-        long initialBalance
-    );
+        String issuerId,
+        LocalDateTime startDate,
+        LocalDateTime endDate
+    ) {
+        var cards = cardRepository
+            .findCards(
+                status,
+                bin,
+                issuerId,
+                startDate,
+                endDate,
+                PageRequest.of(offset, limit)
+            )
+            .stream()
+            .map(this::cardModelFromEntity)
+            .toList();
 
-    List<Card> createCards(List<CardDraft> data);
+        int total = cardRepository.countCards(status, bin, issuerId, startDate, endDate);
 
-    Card getCard(String pan);
+        return new GetCardsResponse(total, cards);
+    }
 
-    /**
-     * Возвращает список карт с пагинацией и фильтрацией
-     *
-     * @param limit     количество карт на странице
-     * @param offset    смещение
-     * @param status    фильтр по статусу
-     * @param bin       фильтр по bin
-     * @param issuerId  фильтр по IssuerId
-     * @param startDate начало диапазона дат
-     * @param endDate   конец диапазона дат
-     * @return список карт
-     */
-    List<Card> getCards(
-        @Nullable Integer limit,
-        @Nullable Integer offset,
-        @Nullable CardStatus status,
-        @Nullable String bin,
-        @Nullable String issuerId,
-        @Nullable LocalDateTime startDate,
-        @Nullable LocalDateTime endDate
-    );
+    public void patchCard(String pan, PatchCardRequest data) {
+        var card = getCardEntity(pan);
 
-    /**
-     * Частично обновляет параметры карты
-     *
-     * @param pan              PAN карты
-     * @param status           новый статус карты
-     * @param dailyLimit       новый дневной лимит карты
-     * @param monthlyLimit     новый месячный лимит карты
-     * @param availableBalance новый баланс карты
-     * @return измененная карта
-     * @throws CardNotFoundException если карта не найдена
-     */
-    Card patchCard(
-        String pan,
-        @Nullable CardStatus status,
-        @Nullable Long dailyLimit,
-        @Nullable Long monthlyLimit,
-        @Nullable Long availableBalance
-    );
+        if (data.status() != null) {
+            card.setStatus(data.status());
+        }
+        if (data.dailyLimit() != null) {
+            card.setDailyLimit(data.dailyLimit());
+        }
+        if (data.monthlyLimit() != null) {
+            card.setMonthlyLimit(data.monthlyLimit());
+        }
+        if (data.availableBalance() != null) {
+            card.setAvailableBalance(data.availableBalance());
+        }
 
-    /**
-     * Удаляет карту
-     *
-     * @param pan PAN карты
-     * @throws CardNotFoundException если карта не найдена
-     */
-    void deleteCard(String pan);
+        cardRepository.save(card);
+    }
 
-    /**
-     * Возвращает общее количество карт в базе данных
-     *
-     * @return количество карт
-     */
-    long countCardsFiltered();
+    public void deleteCard(String pan) {
+        var card = getCardEntity(pan);
+        cardRepository.delete(card);
+    }
 
-    /**
-     * Считает количество карт, удовлетворяющее фильтрам
-     *
-     * @param status    фильтр по статусу
-     * @param bin       фильтр по bin
-     * @param issuerId  фильтр по IssuerId
-     * @param startDate начало диапазона дат
-     * @param endDate   конец диапазона дат
-     * @return количество карт
-     */
-    long countCardsFiltered(
-        @Nullable CardStatus status,
-        @Nullable String bin,
-        @Nullable String issuerId,
-        @Nullable LocalDateTime startDate,
-        @Nullable LocalDateTime endDate
-    );
+    public long countCards() {
+        return cardRepository.count();
+    }
 
-    /**
-     * Резервирует средства на карте, уменьшая доступный баланс
-     *
-     * @param pan    PAN карты
-     * @param amount размер резервирования
-     * @return измененная карта
-     * @throws CardNotFoundException если карта не найдена
-     */
-    Card reserve(String pan, long amount);
+    public void reserve(String pan, ReserveRequest data) {
+        var card = getCardEntity(pan);
+        card.reserve(data.amount());
+        cardRepository.save(card);
+    }
+
+    private CardEntity getCardEntity(String pan) {
+        return cardRepository
+            .findByPan(pan)
+            .orElseThrow(CardNotFoundException::new);
+    }
+
+    private CardModel cardModelFromEntity(CardEntity entity) {
+        return new CardModel(
+            entity.getId(),
+            entity.getPan(),
+            entity.getBin(),
+            entity.getCardholderName(),
+            entity.getStrExpiryDate(),
+            entity.getStatus().name(),
+            entity.getDailyLimit(),
+            entity.getMonthlyLimit(),
+            entity.getAvailableBalance(),
+            entity.getIssuerId(),
+            entity.getCreatedAt()
+        );
+    }
 }
