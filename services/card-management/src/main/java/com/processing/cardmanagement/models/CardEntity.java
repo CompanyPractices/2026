3f77283a0eec2812398c61932b6c9534b9c23b64
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-08 09:52:27 +0300

    refactor(card-management): made soft deletion via SQLDelete annotation
    # Conflicts:
    #	services/card-management/src/main/java/com/processing/cardmanagement/models/CardEntity.java
    #	services/card-management/src/main/java/com/processing/cardmanagement/services/CardService.java

--- services/card-management/src/main/java/com/processing/cardmanagement/models/CardEntity.java
+++ services/card-management/src/main/java/com/processing/cardmanagement/models/CardEntity.java
@@ -1,13 +1,17 @@
 package com.processing.cardmanagement.models;
 
+import com.processing.cardmanagement.exceptions.InsufficientFundsException;
+import com.processing.common.dto.cardmanagement.CardStatus;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Entity
@@ -19,12 +23,12 @@ import java.util.UUID;
     @Index(name = "idx_cards_created_at", columnList = "created_at")
 })
 @NoArgsConstructor
-@AllArgsConstructor
+@SQLDelete(sql = "UPDATE users SET status = 'DELETED' WHERE id = ?")
 @SQLRestriction("status <> 'DELETED'")
 public class CardEntity {
 
     @Id
-    @Column(updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(length = 16, unique = true, nullable = false)
@@ -36,23 +40,74 @@ public class CardEntity {
     @Column(nullable = false)
     private String cardholderName;
 
-    @Column(nullable = false, length = 4)
-    private String expiryDate;
+    @Setter(AccessLevel.NONE)
+    @Column(name = "expiry_date", nullable = false, length = 4)
+    private String strExpiryDate;
+
+    @Transient
+    private LocalDate expiryDate = null;
 
     @Column(length = 20, nullable = false)
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private CardStatus status = CardStatus.ACTIVE;
 
     @Column(length = 3, nullable = false)
-    private String currencyCode;
+    private String currencyCode = "643";
 
-    private long dailyLimit;
+    private long dailyLimit = 15_000_000;
 
-    private long monthlyLimit;
+    private long monthlyLimit = 300_000_000;
 
-    private long availableBalance;
+    private long availableBalance = 1_000_000;
 
     @Column(length = 10, nullable = false)
     private String issuerId;
 
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDate createdAt = LocalDate.now();
+
+    @Transient
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMyy");
+
+    public CardEntity(
+        String pan,
+        String bin,
+        String cardholderName,
+        String currencyCode,
+        CardStatus status,
+        long dailyLimit,
+        long monthlyLimit,
+        long initialBalance,
+        String issuerId
+    ) {
+        this.pan = pan;
+        this.bin = bin;
+        this.cardholderName = cardholderName;
+        this.currencyCode = currencyCode;
+        this.status = status;
+        this.dailyLimit = dailyLimit;
+        this.monthlyLimit = monthlyLimit;
+        this.availableBalance = initialBalance;
+        this.issuerId = issuerId;
+        setExpiryDate(LocalDate.now().plusYears(3));
+    }
+
+    public LocalDate getExpiryDate() {
+        if (expiryDate == null && strExpiryDate != null) {
+            expiryDate = LocalDate.parse(strExpiryDate, formatter);
+        }
+
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+        this.strExpiryDate = expiryDate.format(formatter);
+    }
+
+    public void reserve(long amount) {
+        if (this.availableBalance < amount) {
+            throw new InsufficientFundsException();
+        }
+        this.availableBalance -= amount;
+    }
 }
