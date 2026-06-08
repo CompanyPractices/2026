8eea38f7a1cb262f2f6c05bd852cd45172ea2a95
KulakovaY <408948@niuitmo.ru>
2026-06-08 12:05:10 +0300

    feat: proxy for web socket in vite

--- services/dashboard/vite.config.ts
+++ services/dashboard/vite.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
+    proxy: {
+      '/ws': {
+        target: 'ws://localhost:8088',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
   },
   build: {
     outDir: 'dist',
