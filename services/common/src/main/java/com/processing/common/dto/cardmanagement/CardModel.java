1a9ddc52f08718088dc1f76f843ef05f4f4a09ca
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-08 11:45:37 +0300

    refactor(card-management): creation date of card is now LocalDateTime

--- services/common/src/main/java/com/processing/common/dto/cardmanagement/CardModel.java
+++ services/common/src/main/java/com/processing/common/dto/cardmanagement/CardModel.java
@@ -3,7 +3,6 @@ package com.processing.common.dto.cardmanagement;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
-import java.time.YearMonth;
 import java.util.UUID;
 
 @Schema(description = "Card data transfer object")
@@ -16,21 +15,10 @@ public record CardModel(
     String bin,
     @Schema(description = "Cardholder name", example = "IVAN IVANOV")
     String cardholderName,
-    @Schema(description = "Expiry date in MMyy format", example = "0629")
-    YearMonth expiryDate,
-    @Schema(
-        description = "Card model status",
-        example = "ACTIVE",
-        allowableValues = {
-            "ACTIVE",
-            "INACTIVE",
-            "BLOCKED",
-            "EXPIRED"
-        }
-    )
-    CardModelStatus status,
-    @Schema(description = "Currency code", example = "643")
-    String currencyCode,
+    @Schema(description = "Expiry date in MMYY format", example = "0629")
+    String expiryDate,
+    @Schema(description = "Card status", example = "ACTIVE", allowableValues = {"ACTIVE", "INACTIVE", "BLOCKED", "EXPIRED", "DELETED"})
+    String status,
     @Schema(description = "Daily limit in kopecks", example = "15000000")
     long dailyLimit,
     @Schema(description = "Monthly limit in kopecks", example = "300000000")
