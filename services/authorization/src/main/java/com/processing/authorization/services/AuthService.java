cdb3be17dd95869fe9344854f0384a60f882d785
dermolaeva <darerm1@yandex.ru>
2026-06-09 15:57:01 +0300

    style: added newline to application.yaml

--- services/authorization/src/main/java/com/processing/authorization/services/AuthService.java
+++ services/authorization/src/main/java/com/processing/authorization/services/AuthService.java
@@ -7,8 +7,6 @@ import com.processing.common.dto.cardmanagement.CardModel;
 import com.processing.authorization.dto.ReserveRequest;
 import com.processing.authorization.entities.LimitUsage;
 import com.processing.authorization.constants.CardStatus;
-import com.processing.authorization.entities.LimitUsage;
-import com.processing.authorization.enums.CardStatus;
 import com.processing.authorization.exceptions.CardNotFoundException;
 import com.processing.authorization.exceptions.ReserveCardException;
 
