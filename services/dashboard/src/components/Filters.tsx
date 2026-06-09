b4a8bc6c2b17b5e2bf4a784657854876833461d7
Alina Ramazanova <alinaramazanova610@gmail.com>
2026-06-09 11:41:05 +0300

    style(web-dashboard): add basic grid format for filter

--- services/dashboard/src/components/Filters.tsx
+++ services/dashboard/src/components/Filters.tsx
@@ -24,66 +24,76 @@ export function Filters({issuers, mccNames, onSearch}: FilterProps) {
     }
 
     return (
-        <form onSubmit={filterSubmit}>
-            <div>Фильтр по таблице</div>
-            <label htmlFor="status">Статус: </label>
-            <select value={filter.status ?? ''}
-                    onChange={(e) => setFilter({
-                        ...filter,
-                        status: (e.target.value as FilterStatus) || undefined
-                    })}
-                    id="status">
-                <option value="">Выберите статус</option>
-                <option value="APPROVED">Одобренный</option>
-                <option value="DECLINED">Отклоненный</option>
-            </select>
-
-            <label htmlFor="issuer">Банк эмитент: </label>
-            <select value={filter.issuerId ?? ''}
-                    onChange={(e) => setFilter({
-                        ...filter,
-                        issuerId: e.target.value || undefined
-                    })}
-                    id="issuer">
-                <option value="">Выберите эмитента</option>
-                {IssuersId.map((issuerId) => {
-                    return <option key={issuerId} value={issuerId}>{issuers[issuerId] || issuerId}</option>;
-                })}
-            </select>
-
-            <label htmlFor="mcc">MCC: </label>
-            <select value={filter.mcc ?? ''}
-                    onChange={(e) => setFilter({
-                        ...filter,
-                        mcc: e.target.value || undefined
-                    })}
-                    id="mcc">
-                <option value="">Выберите MCC</option>
-                {Mcc.map((mccCode) => {
-                    return <option key={mccCode} value={mccCode}>{mccNames[mccCode] || mccCode}</option>;
-                })}
-            </select>
-
-            <label htmlFor="dateFrom">Начало даты: </label>
-            <input value={filter.dateFrom ?? ''}
-                   onChange={(e) => setFilter({
-                       ...filter,
-                       dateFrom: e.target.value || undefined
-                   })}
-                   id="dateFrom"
-                   type="date"/>
-
-            <label htmlFor="dateTo">Конец даты: </label>
-            <input value={filter.dateTo ?? ''}
-                   onChange={(e) => setFilter({
-                       ...filter,
-                       dateTo: e.target.value || undefined
-                   })}
-                   id="dateTo"
-                   type="date"/>
-
+        <form onSubmit={filterSubmit} className="grid grid-cols-5 gap-4">
+            <div className="col-span-3" >Фильтр по таблице</div>
             <button type="submit">Найти</button>
             <button type="button" onClick={reset}>Сбросить</button>
+
+            <div>
+                <label htmlFor="status">Статус: </label>
+                <select value={filter.status ?? ''}
+                        onChange={(e) => setFilter({
+                            ...filter,
+                            status: (e.target.value as FilterStatus) || undefined
+                        })}
+                        id="status">
+                    <option value="">Выберите статус</option>
+                    <option value="APPROVED">Одобрен</option>
+                    <option value="DECLINED">Отклонен</option>
+                </select>
+            </div>
+
+            <div>
+                <label htmlFor="issuer">Банк эмитент: </label>
+                <select value={filter.issuerId ?? ''}
+                        onChange={(e) => setFilter({
+                            ...filter,
+                            issuerId: e.target.value || undefined
+                        })}
+                        id="issuer">
+                    <option value="">Выберите эмитента</option>
+                    {IssuersId.map((issuerId) => {
+                        return <option key={issuerId} value={issuerId}>{issuers[issuerId] || issuerId}</option>;
+                    })}
+                </select>
+            </div>
+
+            <div>
+                <label htmlFor="mcc">MCC: </label>
+                <select value={filter.mcc ?? ''}
+                        onChange={(e) => setFilter({
+                            ...filter,
+                            mcc: e.target.value || undefined
+                        })}
+                        id="mcc">
+                    <option value="">Выберите MCC</option>
+                    {Mcc.map((mccCode) => {
+                        return <option key={mccCode} value={mccCode}>{mccNames[mccCode] || mccCode}</option>;
+                    })}
+                </select>
+            </div>
+
+            <div>
+                <label htmlFor="dateFrom">Начало даты: </label>
+                <input value={filter.dateFrom ?? ''}
+                       onChange={(e) => setFilter({
+                           ...filter,
+                           dateFrom: e.target.value || undefined
+                       })}
+                       id="dateFrom"
+                       type="date"/>
+            </div>
+
+            <div>
+                <label htmlFor="dateTo">Конец даты: </label>
+                <input value={filter.dateTo ?? ''}
+                       onChange={(e) => setFilter({
+                           ...filter,
+                           dateTo: e.target.value || undefined
+                       })}
+                       id="dateTo"
+                       type="date"/>
+            </div>
         </form>
     )
 }
