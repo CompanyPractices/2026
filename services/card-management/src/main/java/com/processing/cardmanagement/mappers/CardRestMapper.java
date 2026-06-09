3cc187441a635322d3b343c887c51fe89404394f
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-09 11:09:11 +0300

    fix(card-management): tests run successfully

--- services/card-management/src/main/java/com/processing/cardmanagement/mappers/CardRestMapper.java
+++ services/card-management/src/main/java/com/processing/cardmanagement/mappers/CardRestMapper.java
@@ -3,11 +3,13 @@ package com.processing.cardmanagement.mappers;
 import com.processing.cardmanagement.models.Card;
 import com.processing.common.dto.cardmanagement.CardModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = CardDateMapper.class)
 public interface CardRestMapper {
 
     CardModel toDto(Card card);
 
+    @Mapping(target = "withReserved", ignore = true)
     Card toDomain(CardModel model);
 }
