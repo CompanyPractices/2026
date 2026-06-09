46c009ba4dbe2d03fae3b16fab1950ddb19ec91f
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-09 09:02:02 +0300

    refactor(card-management): card management crud hexagonal architecture

--- services/card-management/src/main/java/com/processing/cardmanagement/services/CardUseCase.java
+++ services/card-management/src/main/java/com/processing/cardmanagement/services/CardUseCase.java
@@ -1,4 +1,57 @@
 package com.processing.cardmanagement.services;
 
+import com.processing.cardmanagement.models.Card;
+import com.processing.cardmanagement.models.CardDraft;
+import com.processing.common.dto.cardmanagement.CardStatus;
+import jakarta.annotation.Nullable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CardUseCase {
+
+    Card createCard(
+        String bin,
+        String cardholderName,
+        String currencyCode,
+        long dailyLimit,
+        long monthlyLimit,
+        long initialBalance
+    );
+
+    List<Card> createGeneratedCards(List<CardDraft> data);
+
+    Card getCard(String pan);
+
+    List<Card> getCards(
+        @Nullable Integer limit,
+        @Nullable Integer offset,
+        @Nullable CardStatus status,
+        @Nullable String bin,
+        @Nullable String issuerId,
+        @Nullable LocalDateTime startDate,
+        @Nullable LocalDateTime endDate
+    );
+
+    void patchCard(
+        String pan,
+        @Nullable CardStatus status,
+        @Nullable Long dailyLimit,
+        @Nullable Long monthlyLimit,
+        @Nullable Long availableBalance
+    );
+
+    void deleteCard(String pan);
+
+    long countCards();
+
+    long countCards(
+        @Nullable CardStatus status,
+        @Nullable String bin,
+        @Nullable String issuerId,
+        @Nullable LocalDateTime startDate,
+        @Nullable LocalDateTime endDate
+    );
+
+    void reserve(String pan, long amount);
 }
