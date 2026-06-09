75725cce71b10bd7c37983845f4978928ceabdaa
TheDenFire <thedenfire@icloud.com>
2026-06-09 11:01:36 +0300

    refactor(merchant acquirer): replace restTemplate to restClient

--- services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/client/GatewayClient.java
+++ services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/client/GatewayClient.java
@@ -5,25 +5,47 @@ import com.processing.merchantacquirer.client.dto.CardsResponse;
 import com.processing.merchantacquirer.domain.model.AuthorizationRequest;
 import com.processing.merchantacquirer.domain.model.AuthorizationResponse;
 import com.processing.merchantacquirer.exception.ExternalServiceException;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Component
-@RequiredArgsConstructor
 public class GatewayClient {
-  private final RestTemplate restTemplate = new RestTemplate();
-  public final String gatewayUrl = "http://gateway:8080";
+  private final RestClient restClient;
+
+  public GatewayClient(RestClient.Builder builder, @Value("${gateway.url}") String gatewayUrl){
+    this.restClient = builder.baseUrl(gatewayUrl).build();
+  }
 
   public CardsResponse getCards(CardsRequest request) {
-    String url = gatewayUrl + "/api/cards";
-    if (request.limit() > 0) {
-      url += "?limit=" + request.limit();
-    }
     try {
-      return restTemplate.getForEntity(url, CardsResponse.class).getBody();
+      log.info(restClient.toString());
+      return restClient.get()
+              .uri(uriBuilder -> {
+                uriBuilder.path("/api/cards");
+                if(request.limit() > 0){
+                  uriBuilder.queryParam("limit", request.limit());
+                }
+                if(request.offset() > 0){
+                  uriBuilder.queryParam("offset", request.offset());
+                }
+                if(request.status() != null){
+                  uriBuilder.queryParam("status", request.status());
+                }
+                if(request.bin() != null){
+                  uriBuilder.queryParam("bin", request.status());
+                }
+                return uriBuilder.build();
+              })
+              .retrieve()
+              .body(CardsResponse.class);
     } catch (HttpClientErrorException | HttpServerErrorException ex) {
       throw ExternalServiceException.fromResponse(ex);
     } catch (Exception ex) {
@@ -32,11 +54,13 @@ public class GatewayClient {
   }
 
   public AuthorizationResponse processAuthorize(AuthorizationRequest authorizationRequest) {
-    String url = gatewayUrl + "/api/transactions";
     try {
-      return restTemplate
-          .postForEntity(url, authorizationRequest, AuthorizationResponse.class)
-          .getBody();
+      return restClient
+          .post()
+              .uri("/api/transactions")
+              .body(authorizationRequest)
+              .retrieve()
+              .body(AuthorizationResponse.class);
     } catch (HttpClientErrorException | HttpServerErrorException ex) {
       throw ExternalServiceException.fromResponse(ex);
     } catch (Exception ex) {
