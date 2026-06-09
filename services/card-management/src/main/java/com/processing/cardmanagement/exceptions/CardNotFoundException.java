b045bb5e97e2b3b0bce9189559ae9b6a53049e2e
daniil3006 <daniil.stank@gmail.com>
2026-06-09 16:43:06 +0300

    fix(card-management): remove redundant Javadoc, replace harcode names with Faker

--- services/card-management/src/main/java/com/processing/cardmanagement/exceptions/CardNotFoundException.java
+++ services/card-management/src/main/java/com/processing/cardmanagement/exceptions/CardNotFoundException.java
@@ -1,8 +1,5 @@
 package com.processing.cardmanagement.exceptions;
 
-/**
- * Выбрасывается, когда карта с указанным PAN не найдена в базе данных
- */
 public final class CardNotFoundException extends CardManagementException {
 
     public CardNotFoundException(String maskedPan) {
