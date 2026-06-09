697daf3ece0892eeade1c019c059f7a45cce500f
daniil3006 <daniil.stank@gmail.com>
2026-06-09 16:07:31 +0300

    test(card-management): add integration test for CardGeneratorController

--- services/card-management/src/test/java/com/processing/cardmanagement/controllers/CardGeneratorControllerIntegrationTest.java
+++ services/card-management/src/test/java/com/processing/cardmanagement/controllers/CardGeneratorControllerIntegrationTest.java
@@ -1,76 +1,76 @@
 package com.processing.cardmanagement.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.processing.cardmanagement.repositories.CardJpaRepository;
+import com.processing.cardmanagement.repositories.CardRepository;
 import com.processing.common.dto.cardmanagement.GenerateCardsRequest;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
-import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest()
+@AutoConfigureMockMvc
 @Testcontainers
 public class CardGeneratorControllerIntegrationTest {
 
-    private static final String POSTGRES_IMAGE = "postgres:16-alpine";
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Autowired
-    private CardJpaRepository cardJpaRepository;
+    private CardRepository cardRepository;
 
     @Container
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(POSTGRES_IMAGE);
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
-    @Value("${local.server.port}")
-    private int port;
-
-    @AfterEach
-    void cleanUp() {
-        cardJpaRepository.deleteAll();
-    }
+//    @AfterEach
+//    void cleanUp() {
+//        cardRepository.
+//    }
 
     @Test
     void generateShouldSaveCardsToDatabaseAndReturn201() throws Exception {
-        int count = 100;
+        int count = 10;
         List<String> bins = List.of("400000", "400001");
         GenerateCardsRequest request = new GenerateCardsRequest(count, bins);
 
-        given()
-                .contentType(ContentType.JSON)
-                .body(objectMapper.writeValueAsString(request))
-                .port(port)
-                .when()
-                .post("/api/cards/generate")
-                .then()
-                .statusCode(201)
-                .extract()
-                .jsonPath();
+        mockMvc.perform(post("/api/cards/generate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.generated").value(10))
+                .andExpect(jsonPath("$.cards", hasSize(10)));
+
+        long dbCount = cardRepository.countCards();
+        assertEquals(2, dbCount);
     }
 
     @Test
     void generateShouldReturn400WhenCountIsZero() throws Exception {
         GenerateCardsRequest request = new GenerateCardsRequest(0, List.of("400000"));
 
-        given()
-                .contentType(ContentType.JSON)
-                .body(objectMapper.writeValueAsString(request))
-                .port(port)
-                .when()
-                .post("/api/cards/generate")
-                .then()
-                .statusCode(400);
+        mockMvc.perform(post("/api/cards/generate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
     }
 }
