eba18ae35e83ad7b6ed445e99b7d24368e5e11c9
Nikita Kapustkin <nkapustkin@openintegration.inc>
2026-06-09 13:49:41 +0300

    test(logger-search): add config for integration test

---
+++ services/transaction-logger/src/test/java/com/processing/TestContainersConfig.java
@@ -0,0 +1,16 @@
+package com.processing;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@Configuration(proxyBeanMethods = false)
+public class TestContainersConfig {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSqlContainer() {
+        return new PostgreSQLContainer<>("postgres:16");
+    }
+}
