dc32ffa36c5832dad1ea0f7efdc7b05f9df1c8d0
Igor Nasonkov <igornasonkov@gmail.com>
2026-06-09 13:48:07 +0300

    refactor(gateway): removed openapi creation logic, replaced with openapi.yaml

--- services/gateway/src/main/java/com/processing/gateway/Application.java
+++ services/gateway/src/main/java/com/processing/gateway/Application.java
@@ -1,13 +1,12 @@
 package com.processing.gateway;
 
-import com.processing.gateway.properties.OpenApiProperties;
 import com.processing.gateway.properties.ServiceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({ServiceProperties.class, OpenApiProperties.class})
+@EnableConfigurationProperties({ServiceProperties.class})
 public class Application {
 
     public static void main(String[] args) {
