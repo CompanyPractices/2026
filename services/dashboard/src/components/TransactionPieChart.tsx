e48ce5d75c767c6849db685aededa23b664ce887
Alina Ramazanova <alinaramazanova610@gmail.com>
2026-06-10 12:31:22 +0300

    feat(web-dashboard): connect web socket to pie chart component

--- services/dashboard/src/components/TransactionPieChart.tsx
+++ services/dashboard/src/components/TransactionPieChart.tsx
@@ -1,55 +1,52 @@
-// import useWebSocket from '../hooks/useWebSocket'
+import useWebSocket from '../hooks/useWebSocket'
 import { Pie, PieChart, Tooltip, Cell, Legend, ResponsiveContainer } from 'recharts';
+import { Transaction } from '../types/index.ts'
 
-export default function TransactionPieChart() {
-    // const {liveTransactions, isConnected} = useWebSocket();
-    const approved = 100;
-    const declined = 200;
-    //
-    // if (!isConnected) {
-    //     return (
-    //         <div>Нет подключения</div>
-    //     )
-    // }
-    //
-    // liveTransactions.Map(transaction => {
-    //     if (transaction.status == 'APPROVED') approved++;
-    //     if (transaction.status == 'DECLINED') declined++;
-    // })
+type PieChartProps = {
+    transactions: Transaction[];
+    isConnected: Boolean;
+}
+export default function TransactionPieChart({transactions, isConnected}: PieChartProps) {
+    const approved = transactions?.filter((s) => s.status === 'APPROVED').length || 0;
+    const declined = transactions?.filter((s) => s.status === 'DECLINED').length || 0;
 
     return (
         <ResponsiveContainer width="80%" height={300} className="mx-auto my-auto" >
-            <PieChart>
-                <Pie
-                    percent
-                    data={[
-                        { name: 'APPROVED', value: approved },
-                        { name: 'DECLINED', value: declined },
-                    ]}
-                    dataKey="value" isAnimationActive={true}
-                    cy="40%"
-                >
-                    <Cell fill="red" />
-                    <Cell fill="lawnGreen" />
-                </Pie>
-                <Legend
-                    layout="horizontal"
-                    align="center"
-                    verticalAlign="bottom"
-                    iconType="circle"
-                    formatter={(value) => {
-                        const count = value === 'APPROVED' ? approved : declined;
-                        const label = value === 'APPROVED' ? 'Одобрено' : 'Отклонено';
-                        return (
-                            <span className="text-zinc-700 font-mono">
+            {!isConnected &&
+                <div className="grid place-content-center text-zinc-700 font-mono" >Ожидание транзакций...</div>
+            }
+            {isConnected &&
+                <PieChart>
+                    <Pie
+                        data={[
+                            { name: 'APPROVED', value: approved },
+                            { name: 'DECLINED', value: declined },
+                        ]}
+                        dataKey="value" isAnimationActive={true}
+                        cy="40%"
+                    >
+                        <Cell fill="red" />
+                        <Cell fill="lawnGreen" />
+                    </Pie>
+                    <Legend
+                        layout="horizontal"
+                        align="center"
+                        verticalAlign="bottom"
+                        iconType="circle"
+                        formatter={(value) => {
+                            const count = value === 'APPROVED' ? approved : declined;
+                            const label = value === 'APPROVED' ? 'Одобрено' : 'Отклонено';
+                            return (
+                                <span className="text-zinc-700 font-mono">
                                 { `${label}(${count})`}
                             </span>
-                        )
+                            )
                         }
-                    }
-                />
-                <Tooltip/>
-            </PieChart>
+                        }
+                    />
+                    <Tooltip/>
+                </PieChart>
+            }
         </ResponsiveContainer>
     )
 }
