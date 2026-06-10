0517ff036f1b2739db7d6887ba87606642473adc
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-10 13:48:37 +0300

    fix(card-management): changed namings for CriteriaBuilderRepository because of magic gnomes

--- services/card-management/src/main/java/com/processing/cardmanagement/repositories/CardCriteriaBuilderJpaRepositoryImpl.java
+++ services/card-management/src/main/java/com/processing/cardmanagement/repositories/CardCriteriaBuilderJpaRepositoryImpl.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CardCriteriaBuilderJpaRepositoryImpl
-    implements CardCriteriaBuilderRepository {
+    implements CardCriteriaBuilderJpaRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
