d2de5e562f1ba2321c4495750401f22333fc6bb8
TheDenFire <thedenfire@icloud.com>
2026-06-10 12:24:22 +0300

    fix(merchant acquirer): fix size symbol

--- services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/controller/dto/AcquirerFeeRequest.java
+++ services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/controller/dto/AcquirerFeeRequest.java
@@ -3,6 +3,6 @@ package com.processing.merchantacquirer.controller.dto;
 public record AcquirerFeeRequest(
         String stan,
         String pan,
-        String terminalID
+        String terminalId
 ){
 }
