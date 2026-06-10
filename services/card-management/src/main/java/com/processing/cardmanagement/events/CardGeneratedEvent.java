9b75777dfafa64b07700ea56fe046cb8423103e1
daniil3006 <daniil.stank@gmail.com>
2026-06-10 15:13:24 +0300

    refactor(card-management) extract metrics to event listener

--- services/card-management/src/main/java/com/processing/cardmanagement/events/CardGeneratedEvent.java
+++ services/card-management/src/main/java/com/processing/cardmanagement/events/CardGeneratedEvent.java
@@ -1,6 +1,6 @@
 package com.processing.cardmanagement.events;
 
-import com.processing.cardmanagement.models.CardStatus;
+import com.processing.common.dto.cardmanagement.CardStatus;
 
 public record CardGeneratedEvent(CardStatus status) {
 }
