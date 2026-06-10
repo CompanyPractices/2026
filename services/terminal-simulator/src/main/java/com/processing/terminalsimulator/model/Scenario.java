10e93d9bdadf07995d09665bbafb06008c4011b4
Elena Serebrennikova <94110281+Umb117@users.noreply.github.com>
2026-06-10 18:22:37 +0300

    Refactor/terminal simulator service&tests (#143)
    * style(terminal-simulator): value in enums from new line, assertThat + isEqualTo replaced on assertEquals, removed print to console from service
    
    * fix(terminal-simulator): removed check for BLOCKED cards from scenarios without blocked cards
    
    * fix(terminal-simulator): urls in application.yml and GatewayClient
    
    * refactor(terminal-simulator): implement TransactionFactory and strategy pattern for transaction types. Introduce TransactionType enum. Add utility classes (StanGenerator, DateTimeGenerator). Refactor TerminalSimulatorService
    
    * tests(terminal-simulator): fixed urls in client tests, moved stan generator tests to a separate class. Changed service tests for new architecture
    
    * fix(terminal-simulator): added @Component on strategies
    
    * fix(terminal-simulator): changed some fields in AuthorizationRequest
    
    * fix(terminal-simulator): changed some fields in AuthorizationRequest
    
    * fix(terminal-simulator): removed Z from date
    
    * style(terminal-simulator): applied pre-commits on e2e folders files

--- services/terminal-simulator/src/main/java/com/processing/terminalsimulator/model/Scenario.java
+++ services/terminal-simulator/src/main/java/com/processing/terminalsimulator/model/Scenario.java
@@ -1,5 +1,9 @@
 package com.processing.terminalsimulator.model;
 
 public enum Scenario {
-    normal, mixed, high_value, night_time, declines_test
+    normal,
+    mixed,
+    high_value,
+    night_time,
+    declines_test
 }
