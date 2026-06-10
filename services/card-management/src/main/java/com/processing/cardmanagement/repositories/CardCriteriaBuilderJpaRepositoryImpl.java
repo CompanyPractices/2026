ac26468fd37cc82c551e4f37ef00f55fed5bb94b
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-10 13:39:21 +0300

    refactor(card-management): implemented filters via CriteriaBuilder (wip, not tested yet)

--- services/card-management/src/main/java/com/processing/cardmanagement/repositories/CardCriteriaBuilderJpaRepositoryImpl.java
+++ services/card-management/src/main/java/com/processing/cardmanagement/repositories/CardCriteriaBuilderJpaRepositoryImpl.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CardCriteriaBuilderJpaRepositoryImpl
-    implements CardCriteriaBuilderJpaRepository {
+    implements CardCriteriaBuilderRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
