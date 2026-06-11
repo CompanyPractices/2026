1d2aa619e4320330145b8da316235707c9237336
Vladimir Rubakhin <vrubakhin@openintegration.inc>
2026-06-11 14:27:11 +0300

    refactor(card-management): made Settings and Defaults for crud service interfaces

--- services/card-management/src/main/java/com/processing/cardmanagement/options/CardServiceDefaults.java
+++ services/card-management/src/main/java/com/processing/cardmanagement/options/CardServiceDefaults.java
@@ -2,19 +2,36 @@ package com.processing.cardmanagement.options;
 
 /**
  * Значения по умолчанию для CardService
- *
- * @param pageLimit    значение лимита для пагинации
- * @param pageOffset   значение сдвига для пагинации
- * @param currencyCode код валюты
- * @param dailyLimit   дневной лимит карты
- * @param monthlyLimit месячный лимит карты
- * @param balance      баланс карты
  */
-public record CardServiceDefaults(
-    long pageLimit,
-    long pageOffset,
-    String currencyCode,
-    long dailyLimit,
-    long monthlyLimit,
-    long balance
-) {}
+public interface CardServiceDefaults {
+
+    /**
+     * @return значение лимита для пагинации
+     */
+    long pageLimit();
+
+    /**
+     * @return значение сдвига для пагинации
+     */
+    long pageOffset();
+
+    /**
+     * @return код валюты
+     */
+    String currencyCode();
+
+    /**
+     * @return дневной лимит карты
+     */
+    long dailyLimit();
+
+    /**
+     * @return месячный лимит карты
+     */
+    long monthlyLimit();
+
+    /**
+     * @return баланс карты
+     */
+    long balance();
+}
