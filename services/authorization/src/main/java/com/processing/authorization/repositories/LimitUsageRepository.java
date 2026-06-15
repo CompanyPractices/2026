cdc803a7350f42150a93754862595be064f0475e
RainyNoodle <408789@niuitmo.ru>
2026-06-15 18:34:22 +0300

    fix: delete unused import

--- services/authorization/src/main/java/com/processing/authorization/repositories/LimitUsageRepository.java
+++ services/authorization/src/main/java/com/processing/authorization/repositories/LimitUsageRepository.java
@@ -4,7 +4,6 @@ import com.processing.authorization.entities.LimitUsage;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
