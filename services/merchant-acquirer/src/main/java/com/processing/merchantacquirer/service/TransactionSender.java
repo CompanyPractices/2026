0ca0478a94d1b6167cf2e0cfce04102d15548b85
TheDenFire <thedenfire@icloud.com>
2026-06-17 16:05:47 +0300

    feat(merchant acquirer): add tps limit in sender

--- services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/service/TransactionSender.java
+++ services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/service/TransactionSender.java
@@ -7,10 +7,13 @@ import com.processing.merchantacquirer.metrics.TransactionMetrics;
 import com.processing.merchantacquirer.service.dto.SimulatorStats;
 import com.processing.common.dto.authorization.AuthorizationRequest;
 import com.processing.common.dto.authorization.AuthorizationResponse;
+
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
+import io.github.bucket4j.Bucket;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -21,14 +24,22 @@ public class TransactionSender {
   private final GatewayClient gatewayClient;
   private final TransactionMetrics transactionMetrics;
   private final Semaphore semaphore;
+  private final Bucket bucket;
 
   public TransactionSender(GatewayClient gatewayClient,
                            TransactionMetrics transactionMetrics,
-                           @Value("${simulation.sender.concurrency}") int concurrency) {
+                           @Value("${simulation.sender.concurrency}") int concurrency,
+                           @Value("${simulation.sender.tps}") int tps) {
     this.gatewayClient = gatewayClient;
     this.transactionMetrics = transactionMetrics;
     this.semaphore = new Semaphore(concurrency);
-    log.info("Concurrency: {}", concurrency);
+    this.bucket = Bucket.builder()
+            .addLimit(limit -> limit
+                    .capacity(tps)
+                    .refillIntervally(tps, Duration.ofSeconds(1))
+            )
+            .build();
+    log.info("Initialized with Concurrency: {} and Target TPS: {}", concurrency, tps);
   }
 
   public SimulatorStats sendAll(List<AuthorizationRequest> requests) {
@@ -42,6 +53,8 @@ public class TransactionSender {
           futures.add(executor.submit(
                   () -> {
                     try {
+                      bucket.asBlocking().consume(1);
+
                       AuthorizationResponse response = gatewayClient.processAuthorize(request);
                       transactionMetrics.record(request.mcc(), response.status());
                       log.info("AuthorizationResponse: {}", response);
