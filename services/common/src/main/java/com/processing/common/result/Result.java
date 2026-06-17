c54ae239e2e2f806a1ad53ec94c9e4deb45026b8
Igor Nasonkov <igornasonkov@gmail.com>
2026-06-17 14:09:25 +0300

    fix(common): fixed lint errors

--- services/common/src/main/java/com/processing/common/result/Result.java
+++ services/common/src/main/java/com/processing/common/result/Result.java
@@ -30,6 +30,6 @@ package com.processing.common.result;
  * @param <E> the type of the error in case of a failed result
  */
 public sealed interface Result<T, E> {
-    record Success<T, E> (T value) implements Result<T, E> {}
-    record Failure<T, E> (E error) implements Result<T, E> {}
+    record Success<T, E>(T value) implements Result<T, E> {}
+    record Failure<T, E>(E error) implements Result<T, E> {}
 }
