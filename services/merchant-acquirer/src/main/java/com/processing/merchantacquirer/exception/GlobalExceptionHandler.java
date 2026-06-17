8da55769d95a4f4dbac63cf01fcdfe7a759fed60
TheDenFire <thedenfire@icloud.com>
2026-06-17 16:49:25 +0300

    fix(merchant acquirer): delete unused import

--- services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/exception/GlobalExceptionHandler.java
+++ services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.processing.merchantacquirer.exception;
 import com.processing.common.dto.ErrorResponse;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
