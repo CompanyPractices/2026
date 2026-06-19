004f216d62f5542924fb0dd735f036bbeafffaf3
daniil3006 <daniil.stank@gmail.com>
2026-06-19 10:59:43 +0300

    fix(e2e-tests): fix TC_15

--- services/e2e-tests/src/test/java/com/processing/e2e/tests/TC_15_ReserveFunds.java
+++ services/e2e-tests/src/test/java/com/processing/e2e/tests/TC_15_ReserveFunds.java
@@ -4,6 +4,7 @@ import com.processing.common.dto.cardmanagement.CardModel;
 import com.processing.common.dto.cardmanagement.ReserveRequest;
 import com.processing.e2e.E2EBaseTest;
 import com.processing.e2e.utility.DBUtils;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
@@ -13,12 +14,21 @@ import static org.testng.Assert.assertEquals;
 public class TC_15_ReserveFunds extends E2EBaseTest {
     private final DBUtils dbUtils = new DBUtils();
 
+    private static final BigDecimal FIRST_RESERVE = new BigDecimal(500000);
+    private static final BigDecimal SECOND_RESERVE = new BigDecimal(300000);
+    private static final String FIRST_RRN = "123456789012";
+    private static final String SECOND_RRN = "123456789013";
+
+    @AfterMethod()
+    public void tearDown() {
+        try {
+            dbUtils.executeUpdate("DELETE FROM reservations WHERE rrn IN (?, ?)", FIRST_RRN, SECOND_RRN);
+        } catch (Exception ignored) {
+        }
+    }
+
     @Test(description = "TC-15 -  Резервирование средств (POST /api/cards/{pan}/reserve)")
     public void tc15_reserveFunds() throws Exception {
-        BigDecimal firstReserve = new BigDecimal(500000);
-        BigDecimal secondReserve = new BigDecimal(300000);
-        String firstRrn = "123456789012";
-        String secondRrn = "123456789013";
 
         CardModel createdCard = mapper.treeToValue(
                 httpPost(GATEWAY_URL, "/api/cards", createCardRequest, 201),
@@ -29,8 +39,8 @@ public class TC_15_ReserveFunds extends E2EBaseTest {
         assertEquals(balanceBefore.compareTo(createdCard.availableBalance()), 0);
 
         httpUtils.httpPost(GATEWAY_URL, "/api/cards/" + createdCard.pan() + "/reserve",
-                new ReserveRequest(firstReserve, firstRrn), 200);
-        BigDecimal expectedAfterFirst = balanceBefore.subtract(firstReserve);
+                new ReserveRequest(FIRST_RESERVE, FIRST_RRN), 200);
+        BigDecimal expectedAfterFirst = balanceBefore.subtract(FIRST_RESERVE);
 
         BigDecimal balanceAfterFirstReserve = new BigDecimal(
                 dbUtils.queryLong("SELECT available_balance FROM cards WHERE pan = ?", createdCard.pan()));
@@ -43,8 +53,8 @@ public class TC_15_ReserveFunds extends E2EBaseTest {
         assertEquals(cardAfterFirstReserve.availableBalance().compareTo(expectedAfterFirst), 0);
 
         httpUtils.httpPost(GATEWAY_URL, "/api/cards/" + createdCard.pan() + "/reserve",
-                new ReserveRequest(secondReserve, secondRrn), 200);
-        BigDecimal expectedAfterSecond = balanceBefore.subtract(firstReserve).subtract(secondReserve);
+                new ReserveRequest(SECOND_RESERVE, SECOND_RRN), 200);
+        BigDecimal expectedAfterSecond = balanceBefore.subtract(FIRST_RESERVE).subtract(SECOND_RESERVE);
 
         BigDecimal balanceAfterSecondReserve = new BigDecimal(
                 dbUtils.queryLong("SELECT available_balance FROM cards WHERE pan = ?", createdCard.pan()));
