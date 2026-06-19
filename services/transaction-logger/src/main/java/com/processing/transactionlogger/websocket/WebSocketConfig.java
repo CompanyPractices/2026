ebde2944f39069ac8662f629cb592f225c627606
KulakovaY <408948@niuitmo.ru>
2026-06-19 10:09:55 +0300

    fix: allowOrigins

--- services/transaction-logger/src/main/java/com/processing/transactionlogger/websocket/WebSocketConfig.java
+++ services/transaction-logger/src/main/java/com/processing/transactionlogger/websocket/WebSocketConfig.java
@@ -18,7 +18,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     private final TransactionWebSocketHandler handler;
     @Value("${websocket.allowed-origins}")
-    private String allowedOrigins;
+    private String[] allowedOrigins;
 
     @Override
     public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
