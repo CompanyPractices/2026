aeab0a594a7ca0eb8e17a95a34424005ccb08e76
Alina Ramazanova <alinaramazanova610@gmail.com>
2026-06-22 14:46:25 +0300

    feat(web-dashboard): add animation for toasts

--- services/dashboard/src/components/ToastContainer.tsx
+++ services/dashboard/src/components/ToastContainer.tsx
@@ -1,5 +1,6 @@
 import { Toast } from '../types/toast.ts'
 import { ToastItem } from './ToastItem.tsx'
+import { AnimatePresence } from 'framer-motion'
 
 type ToastContainerProps = {
     toasts: Toast[];
@@ -10,11 +11,19 @@ export function ToastContainer({toasts, onClose}: ToastContainerProps){
     return (
         <div className="fixed top-4 right-4 z-[9999] flex flex-col gap-3 pointer-events-none
                         max-h-[80vh] overflow-y-auto w-full max-w-sm sm:max-w-md">
-            {toasts.map((ts) => (
-                <div key={ts.id} className="pointer-events-auto">
-                    <ToastItem toast={ts} onClose={onclose} />
-                </div>
-            ))}
+            <AnimatePresence>
+                {toasts.map((ts) => (
+                    <motion.div
+                        initial={{ opacity: 0, height: 0 }}
+                        animate={{ opacity: 1, height: "auto" }}
+                        exit={{ opacity: 0, height: 0 }}
+                        transition={{ duration: 0.3 }}
+                        key={ts.id}
+                        className="pointer-events-auto">
+                        <ToastItem toast={ts} onClose={onclose} />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     )
 }
