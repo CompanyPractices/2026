9374e2b253ecc935734a78044a76bcabf92c1445
Alina Ramazanova <alinaramazanova610@gmail.com>
2026-06-22 14:46:00 +0300

    feat(web-dashboard): add timers for toasts

--- services/dashboard/src/contexts/ToastProvider.tsx
+++ services/dashboard/src/contexts/ToastProvider.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, ReactNode } from 'react'
+import { useState, useCallback, ReactNode, useRef } from 'react'
 import { Toast, ToastType } from '../types/toast.ts'
 import { ToastContext } from './ToastContext.ts'
 
@@ -8,13 +8,27 @@ interface ToastProviderProps {
 
 export function ToastProvider ({children}: ToastProviderProps) {
     const [toasts, setToasts] = useState<Toast[]>([]);
+    const timers = useRef<Record<string, ReturnType<typeof setTimeout>>>({});
 
-    const addToast = useCallback((message: string, type: ToastType) => {
-        const newToast: Toast = {id: crypto.randomUUID(), message, type};
-        setToasts(prev => [...prev, newToast]);
+    const clearTimer = useCallback((id: string) => {
+        const currentTimer = timers.current[id];
+        if(currentTimer){
+            clearTimeout(currentTimer);
+            delete timers.current[id]
+        }
     }, []);
 
+    const addToast = useCallback((message: string, type: ToastType) => {
+        const newToast: Toast = {id: crypto.randomUUID(), message, type, duration: 4000};
+        setToasts(prev => [...prev, newToast]);
+
+        timers.current[newToast.id] = setTimeout(() => {
+            removeToast(newToast.id);
+        }, newToast.duration);
+    }, [removeToast]);
+
     const removeToast = useCallback((id: string) => {
+        clearTimer(id);
         setToasts(prev => prev.filter(t => t.id !== id));
     }, []);
 
