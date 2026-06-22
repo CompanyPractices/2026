8cbc2a9441750492b03134bf3a164ab99a310920
TheDenFire <thedenfire@icloud.com>
2026-06-22 15:53:18 +0300

    fix(merchant acquirer): add fix which don't touch external exceptions

--- services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/exception/GlobalExceptionHandler.java
+++ services/merchant-acquirer/src/main/java/com/processing/merchantacquirer/exception/GlobalExceptionHandler.java
@@ -6,13 +6,15 @@ import java.time.Instant;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.processing.merchantacquirer.controller")
 public class GlobalExceptionHandler {
   private static final String SERVICE_NAME = "Merchant acquirer simulator";
   private static final String NO_RETRY = "0";
